48b277a90888fdb36f232cb8a7d7c271f4ab9df3
Pranav <pranav.techiegeek@gmail.com>
2024-01-01 16:46:44 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import React from "react";
-import logo from "../assets/tesla.svg";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <h5>Model S</h5>
-          <h5>Model 3</h5>
-          <h5>Model X</h5>
-          <h5>Model Y</h5>
-          <h5>Solar Roof</h5>
-          <h5>Solar Panels</h5>
-        </nav>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
